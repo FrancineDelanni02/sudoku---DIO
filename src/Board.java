64943db5c7cc5a6f5bd22441f9a476845f6904d5
FrancineDelanni02 <delannifrancine@gmail.com>
2025-04-14 14:57:38 -0300

    create logica

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,11 @@
 package src;
 
+import java.util.List;
+
 public class Board {
+    /*Se os nodos forem um "lista linear", então a lógica seria de que uma nova linha seria
+    * iniciada a cada 9 posições, até chegar em 81
+    * Não pode haver o mesmo número na mesma posição em nenhum momento*/
+    private List<Node> nodes;
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,34 @@
 package src;
 
 public class Node {
+    private boolean fixed;
+    private Integer actualNumber;
+    private Integer expectedNumber;
+
+    /*Quando um node for fixed, o actual já é preenchido e o expected é marcado com o actual
+    * automaticamente
+    * Eu não preciso definir que o fixed é true ou false sempre, se eu chamar um construtor de
+    * um nodo vazio que deve ser preenchido ele já coloca o fixed automaticamente como false
+    * Quando o usuário quiser mudar o valor de um nodo, primeiro deve ser verificado se ele é
+    * fixed, se sim não deve ser permitida a modificação
+    * O */
+
+    public Node(boolean fixed,Integer actualNumber){
+        this.actualNumber = actualNumber;
+        if(fixed)
+            this.expectedNumber = this.actualNumber;
+    }
+
+    public Node(){
+        this.fixed = false;
+        this.actualNumber = null;
+        this.expectedNumber = 0;
+    }
+
+    public void setActualNumber(Integer number){
+        if(fixed)
+            return;
+
+        this.actualNumber = number;
+    }
 }
